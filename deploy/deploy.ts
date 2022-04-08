4f37eba8f30a7de86c63aa6b495916ce4150c2d8
Mickey <70309026+hedgezhu@users.noreply.github.com>
2022-04-09 00:44:49 +0800

    feat: evm bytecode & contract abi

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,12 +1,19 @@
 import { utils, Wallet } from "zksync-web3";
 import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+// https://github.com/matter-labs/hardhat-zksync/blob/main/packages/hardhat-zksync-deploy/src/deployer.ts
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
-export default async function (hre: HardhatRuntimeEnvironment) {
-  console.log('run deploy for Greeter contract');
+import { private_key } from '../key/damocles';
 
-  
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log('run deploy code for Greeter contract');
+
+  // 初始化钱包
+  const wallet = new Wallet(private_key);
+
+  const deployer = new Deployer(hre, )
 }
 
 
