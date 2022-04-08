e48f24e8174ce7268f88a8e62d3df4585cf14814
Mickey <70309026+hedgezhu@users.noreply.github.com>
2022-04-08 23:08:00 +0800

    feat: deploy code for contract

---
+++ deploy/deploy.ts
@@ -0,0 +1,15 @@
+import { utils, Wallet } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log('run deploy for Greeter contract');
+
+  
+}
+
+
+
+// import * as x from "zksync-web3";
+console.log(Deployer);
